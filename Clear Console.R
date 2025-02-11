cbf7b1578ce2f7fff4be8a17722f9b50df3697ae
Balaji Muniaswamy <37670327+balajitechguy@users.noreply.github.com>
2025-02-11 16:17:38 +0530

    Add files via upload

---
+++ Clear Console.R
@@ -0,0 +1,9 @@
+# ------------------------------------------------------------------------------
+# Program Name: Clear Console.R
+# Author: Balaji
+# Date: 11-Feb-2025
+# Purpose: Auto clean console during execution
+# ------------------------------------------------------------------------------
+cat("\014")
+
+View(mtcars)
